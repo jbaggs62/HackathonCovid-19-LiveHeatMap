0fdae48f6b0847fd3f7f5e81c394eefb00846c56
Melinda Rabenstein <mrabenstein@webmd.net>
2020-03-29 11:56:50 -0400

    Swap to more vertical layout

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 $(function() {
-	$('#tabsVertical li a:not(:first)').addClass('inactive');
-	$('.content').addClass('hidden');
-	$('.content:first').removeClass('hidden');
+	$('#tabsMainMenu > li:not(:first) > a').addClass('inactive');
+	$('.dataHolder').addClass('hidden');
+	$('.dataHolder:first').removeClass('hidden');
 	    
-	$('#tabsVertical li a').click(function(){
+	$('#tabsMainMenu > li > a').click(function(){
 		var t = $(this).attr('id');
 		if($(this).hasClass('inactive')){ //this is the start of our condition 
-			$('#tabsVertical li a').addClass('inactive');           
+			$('#tabsMainMenu > li > a').addClass('inactive');           
 			$(this).removeClass('inactive');
 	    
-			$('.content').addClass('hidden');
+			$('.dataHolder').addClass('hidden');
 			$('#'+ t + 'C').hide().removeClass('hidden').fadeIn('slow');
 		}
 	});
@@ -18,14 +18,14 @@ $(function() {
 	
 	
 	
-	$('#tabsStates li a:not(:first)').addClass('inactive');
+	$('#tabsStates > li:not(:first) > a').addClass('inactive');
 	$('#stateDataC .data').addClass('hidden');
 	$('#stateDataC .data:first').removeClass('hidden');
 	    
-	$('#tabsStates li a').click(function(){
+	$('#tabsStates > li > a').click(function(){
 		var t = $(this).attr('id');
 		if($(this).hasClass('inactive')){ //this is the start of our condition 
-			$('#tabsStates li a').addClass('inactive');           
+			$('#tabsStates > li > a').addClass('inactive');           
 			$(this).removeClass('inactive');
 	    
 			$('#stateDataC .data').addClass('hidden');
@@ -38,10 +38,23 @@ $(function() {
 	
 	
 	$.getJSON("https://xn57ih8ghl.execute-api.us-east-1.amazonaws.com/prod", function(data) {
-		var containerDiv = document.getElementById("dashboardContainer");
 		var options = {
 				url: data.url,
 				container: document.getElementById("usDashboard"),
+				parameters: {
+					country: 'United States'
+				},
+				scrolling: "no",
+				height: "AutoFit",
+				width: "100%"
+		};
+		dashboard = QuickSightEmbedding.embedDashboard(options);
+	});
+	
+	$.getJSON("https://xn57ih8ghl.execute-api.us-east-1.amazonaws.com/prod", function(data) {
+		var options = {
+				url: data.url,
+				container: document.getElementById("alDashboard"),
 				parameters: {
 					country: 'United States'
 				},
