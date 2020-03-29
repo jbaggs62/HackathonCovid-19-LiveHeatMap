95d83176ef4ba639f92177593621fd65a274d8cf
Melinda Rabenstein <mrabenstein@webmd.net>
2020-03-29 17:44:10 -0400

    Swap states to templated layout

--- js/app.js
+++ js/app.js
@@ -18,6 +18,80 @@ $(function() {
 	
 	
 	
+	var states = $("#tabsStates > li > a");
+	function createSummaryStateView(abbreviation) {
+		return function(data) {
+			var options = {
+					url: data.url,
+					container: document.getElementById(abbreviation + "DashboardSummary"),
+					parameters: {
+						state: abbreviation.toUpperCase()
+					},
+					scrolling: "no",
+					height: "AutoFit",
+					width: "100%"
+			};
+			dashboard = QuickSightEmbedding.embedDashboard(options);
+		}
+	}
+	function createDetailedStateView(abbreviation) {
+		return function(data) {
+			var options = {
+					url: data.url,
+					container: document.getElementById(abbreviation + "DashboardDetail"),
+					parameters: {
+						state: abbreviation.toUpperCase()
+					},
+					scrolling: "no",
+					height: "AutoFit",
+					width: "100%"
+			};
+			dashboard = QuickSightEmbedding.embedDashboard(options);
+		}
+	}
+	$.each(states, function(data) {
+		var abbreviation = data.id.substr(0, 2);
+		var name = data.text;
+		var twitter = data.dataset.twitter;
+		
+		var stateLayout = `<!-- ${name} -->
+		<div id="${abbreviation}DataC" class="data hidden">
+			<div class="splash-container">
+			    <div class="splash">
+			    	<div>
+				        <h1 class="splash-head">
+							<span>Current Status:</span>
+							${name}
+						</h1>
+						
+						<section id="${abbreviation}DashboardSummary" class="post"></section>
+			    	</div>
+					
+					<div>
+				        <a class="twitter-timeline" data-height="500" href="https://twitter.com/${twitter}"></a>
+					</div>
+			    </div>
+			</div>
+			
+			<div class="content-wrapper">
+			    <div class="content">
+			        <h2 class="content-head is-center">${name}</h2>
+			        
+			        <section id="${abbreviation}DashboardDetail" class="post"></section>
+			    </div>
+			</div>
+		</div>`;
+		
+		$("#stateDataC")[0].innerHTML += stateLayout;
+		
+		$.getJSON("https://xn57ih8ghl.execute-api.us-east-1.amazonaws.com/prod/summary", createSummaryStateView(abbreviation));
+		$.getJSON("https://xn57ih8ghl.execute-api.us-east-1.amazonaws.com/prod/county", createDetailedStateView(abbreviation));
+	});
+	
+	
+	
+	
+	
 	$('#tabsStates > li:not(:first) > a').addClass('inactive');
 	$('#stateDataC .data').addClass('hidden');
 	$('#stateDataC .data:first').removeClass('hidden');
@@ -32,6 +106,7 @@ $(function() {
 			$('#'+ t + 'C').hide().removeClass('hidden').fadeIn('slow');
 		}
 	});
+	
 	
 	
 	
@@ -53,34 +128,6 @@ $(function() {
 				url: data.url,
 				container: document.getElementById("usDashboardSummary"),
 				parameters: { },
-				scrolling: "no",
-				height: "AutoFit",
-				width: "100%"
-		};
-		dashboard = QuickSightEmbedding.embedDashboard(options);
-	});
-	
-	// ALABAMA
-	$.getJSON("https://xn57ih8ghl.execute-api.us-east-1.amazonaws.com/prod/summary", function(data) {
-		var options = {
-				url: data.url,
-				container: document.getElementById("alDashboardDetail"),
-				parameters: {
-					state: 'AL'
-				},
-				scrolling: "no",
-				height: "AutoFit",
-				width: "100%"
-		};
-		dashboard = QuickSightEmbedding.embedDashboard(options);
-	});
-	$.getJSON("https://xn57ih8ghl.execute-api.us-east-1.amazonaws.com/prod/details", function(data) {
-		var options = {
-				url: data.url,
-				container: document.getElementById("alDashboardSummary"),
-				parameters: {
-					state: 'AL'
-				},
 				scrolling: "no",
 				height: "AutoFit",
 				width: "100%"
